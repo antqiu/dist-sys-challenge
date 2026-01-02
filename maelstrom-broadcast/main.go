e4333b9d09ed6d5ae35d5489e99cf98b5474b613
Anthony <anthonyqiuchu@gmail.com>
2026-01-01 21:22:27 -0500

    Test case 3C. Added gossip to send entire state

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -3,13 +3,16 @@ package main
 import (
     "encoding/json"
     "log"
+	"time"
 
     maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+
 func main() {
 	n := maelstrom.NewNode()
 	topology := make(map[string][]string)
+	initialized := false
 
 	seen_messages_arr := make([]int, 0)
 	seen_messages := make(map[int]bool) // equivalent of a set
@@ -21,36 +24,60 @@ func main() {
 			return err
 		}
 
-		n_id := n.ID() // node id: string - this must be in the handler because it guarantees that it executes after init
-
+		// n_id := n.ID() // node id: string - this must be in the handler because it guarantees that it executes after init
 		
 		response := make(map[string]any)
 		response["type"] = "broadcast_ok"
 
-		f, ok := body["message"].(float64) // assert that it is at least type float
+		// f, ok := body["message"].(float64) // assert that it is at least type float
 		
-		if ok {
-			in := int(f) // if float, convert to int
-			_, seen_ok := seen_messages[in]
-			if !seen_ok {
-				// append, broadcast
-				seen_messages_arr = append(seen_messages_arr, in)
-				_, topology_ok := topology[n_id]
-				if topology_ok {
-					for _, neighborString := range topology[n_id] {
-						n.Send(neighborString, map[string]any{
-							"type":    "broadcast",
-							"message": in,
-							"broadcasted" : "yes",
-						})
+		// if ok {
+		// 	in := int(f) // if float, convert to int
+		// 	_, seen_ok := seen_messages[in]
+		// 	if !seen_ok {
+		// 		// append, broadcast
+		// 		seen_messages_arr = append(seen_messages_arr, in)
+		// 		_, topology_ok := topology[n_id]
+		// 		if topology_ok {
+		// 			for _, neighborString := range topology[n_id] {
+		// 				n.Send(neighborString, map[string]any{
+		// 					"type":    "broadcast",
+		// 					"message": in,
+		// 				})
+		// 			}
+		// 		}
+		// 		seen_messages[in] = true
+		// 	}
+		// }
+
+		_, ok := body["is_array"]
+		if ok { 
+			// is an array from gossiping, run throughh all of different values and gossip those
+			receiving_arr, ok := body["message"].([]any)
+
+			if (len(receiving_arr) != len(seen_messages_arr) && ok) {
+				for _, v := range receiving_arr {
+					value := int(v.(float64))
+					_, ok = seen_messages[value]
+					if !ok {
+						seen_messages[value] = true 
+						seen_messages_arr = append(seen_messages_arr, value)
 					}
 				}
+			}
+		} else {
+			// single message broadcast
+			f, _ := body["message"].(float64) // assert that it is at least type float
+			in := int(f) // if float, convert to int
+
+			_, seen_ok := seen_messages[in]
+			if !seen_ok {
+				seen_messages_arr = append(seen_messages_arr, in)
 				seen_messages[in] = true
 			}
 		}
-
-		_, broadcasted := body["broadcasted"]
-		if !broadcasted {
+	
+		if msg.Src[0] == 'c' {
 			return n.Reply(msg, response)
 		}
 
@@ -80,6 +107,8 @@ func main() {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+
+		initialized = true
 
 		response := make(map[string]any)
 
@@ -116,8 +145,33 @@ func main() {
 	})
 
 
+	go func() {
+		ticker := time.NewTicker(500 * time.Millisecond)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			if !initialized {
+				continue
+			}
+			gossip(n, topology, seen_messages_arr)
+		}
+	}()
+
+
 	if err := n.Run(); err != nil {
     	log.Fatal(err)
 	}
 
+}
+
+func gossip(n *maelstrom.Node, topology map[string][]string, seen []int) {
+		id := n.ID()
+
+		for _, neighbor := range topology[id] {
+			n.Send(neighbor, map[string]any{
+				"type":    "broadcast",
+				"message": seen,
+				"is_array": "yes",
+			})
+		}
 }
